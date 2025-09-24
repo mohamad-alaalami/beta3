0fa4512c30f4bd6df5676b12b44b88ec70363f9f
MohammadKassira <mhdkassira@gmail.com>
2025-09-24 13:27:46 +0300

    main

--- main.c
+++ main.c
@@ -61,48 +61,54 @@ int check_win_horizontal(char** grid, int x, int y, char player)
 }
 
 int check_win_diagonals(char **grid, int x, int y, char player){
-    int temp_x = x;
+	int temp_x = x;
 	int temp_y = y;
-	int counter = 0;
-    
-	for(int z = y;z < 6;z++){
-        temp_x--;
-		temp_y++;
-	}
+	int counter;
 
-	while(temp_x >= 0 && temp_x < 6 && temp_y >= 0 && temp_y < 7){
-		if(grid[temp_x][temp_y] == player){
-            counter++;
-		}
-		if(counter == 4){
-            return 1;
-		}
-        
+	while(temp_x > 0 && temp_x < 5 && temp_y > 0 && temp_y < 6){
 		temp_x++;
 		temp_y--;
 	}
-    
+
+	counter = 0;
+	while(temp_x >= 0 && temp_x <= 5 && temp_y >= 0 && temp_y <= 6 ){
+		
+		if(grid[temp_x][temp_y] == player){
+			counter++;
+		}
+		else{
+			counter = 0;
+		}
+		if(counter == 4){
+			return 1;
+			}
+		temp_x--;
+		temp_y++;
+	}
 	temp_x = x;
 	temp_y = y;
-	counter = 0;
-    
-	for(int z = y;z < 6;z++){
-        temp_x--;
-		temp_y--;
-	}
 
-	while(temp_x >= 0 && temp_x < 6 && temp_y >= 0 && temp_y < 7){
-		if(grid[temp_x][temp_y] == player){
-            counter++;
-		}
-		if(counter == 4){
-            return 1;
-		}
-
+	while(temp_x > 0 && temp_x < 5 && temp_y > 0 && temp_y < 6 ){
 		temp_x++;
 		temp_y++;
+		
 	}
-    
+
+	counter = 0;
+		while(temp_x >= 0 && temp_x <= 5 && temp_y >= 0 && temp_y <= 6){
+		
+		if(grid[temp_x][temp_y] == player){
+			counter++;
+		}
+		else{
+			counter = 0;
+		}
+		if(counter == 4){
+			return 1;
+			}
+		temp_x--;
+		temp_y--;
+	}
 	return 0;
 }
 
