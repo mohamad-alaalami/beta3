5e1f62a9c904a0512f3060846a4418593080a764
MohammadKassira <mhdkassira@gmail.com>
2025-10-19 16:07:12 +0300

    Formatting

--- player.c
+++ player.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/*function that takes as input the column choice of the user if it is a valid choice(in range and an integer) it checks the capacity of the column and places it in the lowest available slot*/
-int* player_move(char** grid, int* capacities, char player)
-{
 
+/*function that takes as input the column choice of the user if it is a valid choice(in range and an integer) it checks the capacity of the column and places it in the lowest available slot*/
+int* player_move(char** grid, int* capacities, char player){
     int choice;
 
     int* returnpos = (int*)malloc(2*sizeof(int));
-    while(1)
-    {
+    while(1){
         printf("Player %c, please choose a column: ", player);
         if(scanf("%d", &choice) != 1){
             fflush(stdout);
@@ -19,9 +17,7 @@ int* player_move(char** grid, int* capacities, char player)
             continue;
         }
         
-        if(choice > 0 && choice <= COLS)
-        {
-            
+        if(choice > 0 && choice <= COLS){
             if(capacities[choice - 1] < ROWS){
                 int row = ROWS - capacities[choice - 1] - 1;
                 grid[row][choice - 1] = player;
@@ -34,16 +30,13 @@ int* player_move(char** grid, int* capacities, char player)
                 return returnpos;
         }
 
-            else
-            {
+            else{
                 printf("column is full.\n");
             }
         }
 
-        else
-        {
+        else{
             printf("invalid column.\n");
         }
-
     }
 }
